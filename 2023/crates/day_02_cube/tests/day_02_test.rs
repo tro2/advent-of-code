4d4cc2171a948b2128b779c55947ba972c77174a
tro2 <62850247+tro2@users.noreply.github.com>
2024-11-25 09:06:23 -0800

    updated testing function names for easier access to individual tests

--- 2023/crates/day_02_cube/tests/day_02_test.rs
+++ 2023/crates/day_02_cube/tests/day_02_test.rs
@@ -1,21 +1,21 @@
 use day_02;
 
 #[test]
-fn part_01_example() {
+fn d2_part_01_example() {
     assert_eq!(day_02::part_01("tests/day_02_example.txt"), 8);
 }
 
 #[test]
-fn part_01_input() {
+fn d2_part_01_input() {
     assert_eq!(day_02::part_01("tests/day_02_input.txt"), 2541);
 }
 
 #[test]
-fn part_02_example() {
+fn d2_part_02_example() {
     assert_eq!(day_02::part_02("tests/day_02_example.txt"), 2286);
 }
 
 #[test]
-fn part_02_input() {
+fn d2_part_02_input() {
     assert_eq!(day_02::part_02("tests/day_02_input.txt"), 66016);
 }
