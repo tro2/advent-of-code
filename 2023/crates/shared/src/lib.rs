b39d446fb1cea992d41db4537d42ce44bbefc568
tro2 <62850247+tro2@users.noreply.github.com>
2024-11-26 14:58:57 -0800

    line endings

--- 2023/crates/shared/src/lib.rs
+++ 2023/crates/shared/src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Eq, PartialEq, Debug, Clone, Copy)]
-pub struct Coordinate {
+pub struct Coordinates {
     pub x: usize,
     pub y: usize,
 }
@@ -23,7 +23,18 @@ pub enum Offsets {
 }
 
 impl Offsets {
-    pub fn to_offset(&self) -> Offset {
+    pub const ALL: [Offset; 8] = [
+        Offsets::North.to_offset(),
+        Offsets::NorthEast.to_offset(),
+        Offsets::East.to_offset(),
+        Offsets::SouthEast.to_offset(),
+        Offsets::South.to_offset(),
+        Offsets::SouthWest.to_offset(),
+        Offsets::West.to_offset(),
+        Offsets::NorthWest.to_offset(),
+    ];
+
+    pub const fn to_offset(&self) -> Offset {
         match self {
             Offsets::None => Offset { x: 0, y: 0 },
             Offsets::North => Offset { x: 0, y: -1 },
@@ -46,34 +57,110 @@ pub trait Arrayto2DGrid {
         self.array_len() / self.row_len()
     }
 
-    fn idx_to_coords(&self, idx: usize) -> Option<Coordinate> {
+    fn idx_to_coords(&self, idx: usize) -> Option<Coordinates> {
         let x = idx % self.row_len();
         let y = idx / self.row_len();
-        if y >= self.array_len() {
+        if y > self.num_rows() {
             return None;
         }
-        Some(Coordinate { x, y })
+        Some(Coordinates { x, y })
     }
 
-    fn coords_to_idx(&self, coords: Coordinate) -> Option<usize> {
-        if coords.x >= self.row_len() || coords.y >= self.num_rows() {
+    fn coords_to_idx(&self, coords: &Coordinates) -> Option<usize> {
+        if coords.x > self.row_len() || coords.y > self.num_rows() {
             return None;
         }
         Some(coords.y * self.row_len() + coords.x)
     }
 
-    fn translate_coords(&self, coords: Coordinate, offset: Offset) -> Option<Coordinate> {
+    fn translate_coords(&self, coords: &Coordinates, offset: &Offset) -> Option<Coordinates> {
         let x = coords.x as isize + offset.x;
         let y = coords.y as isize + offset.y;
-        if x < 0 || y < 0 || x >= self.row_len() as isize || y >= self.num_rows() as isize {
+        if x < 0 || y < 0 || x > self.row_len() as isize || y > self.num_rows() as isize {
             return None;
         }
-        Some(Coordinate { x: x as usize, y: y as usize })
+        Some(Coordinates { x: x as usize, y: y as usize })
     }
 
-    fn translate_idx(&self, idx: usize, offset: Offset) -> Option<usize> {
+    fn translate_idx(&self, idx: usize, offset: &Offset) -> Option<usize> {
         let coords = self.idx_to_coords(idx)?;
-        let ret = self.translate_coords(coords, offset)?;
-        self.coords_to_idx(ret)
+        let ret = self.translate_coords(&coords, offset)?;
+        self.coords_to_idx(&ret)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Grid {
+        array_len: usize,
+        row_len: usize,
+    }
+
+    impl Arrayto2DGrid for Grid {
+        fn row_len(&self) -> usize {
+            self.row_len
+        }
+
+        fn array_len(&self) -> usize {
+            self.array_len
+        }
+    }
+
+    #[test]
+    fn test_idx_to_coords() {
+        let grid_tall = Grid {
+            array_len: 6,
+            row_len: 2,
+            // num_rows = 6 / 2 = 3
+        };
+        assert_eq!(grid_tall.idx_to_coords(0), Some(Coordinates { x: 0, y: 0 }));
+        assert_eq!(grid_tall.idx_to_coords(1), Some(Coordinates { x: 1, y: 0 }));
+        assert_eq!(grid_tall.idx_to_coords(2), Some(Coordinates { x: 0, y: 1 }));
+        assert_eq!(grid_tall.idx_to_coords(3), Some(Coordinates { x: 1, y: 1 }));
+        assert_eq!(grid_tall.idx_to_coords(4), Some(Coordinates { x: 0, y: 2 }));
+        assert_eq!(grid_tall.idx_to_coords(5), Some(Coordinates { x: 1, y: 2 }));
+
+        let grid_wide = Grid {
+            array_len: 6,
+            row_len: 3,
+            // num_rows = 6 / 3 = 2
+        };
+        assert_eq!(grid_wide.idx_to_coords(0), Some(Coordinates { x: 0, y: 0 }));
+        assert_eq!(grid_wide.idx_to_coords(1), Some(Coordinates { x: 1, y: 0 }));
+        assert_eq!(grid_wide.idx_to_coords(2), Some(Coordinates { x: 2, y: 0 }));
+        assert_eq!(grid_wide.idx_to_coords(3), Some(Coordinates { x: 0, y: 1 }));
+        assert_eq!(grid_wide.idx_to_coords(4), Some(Coordinates { x: 1, y: 1 }));
+        assert_eq!(grid_wide.idx_to_coords(5), Some(Coordinates { x: 2, y: 1 }));
+
+        let grid_square = Grid {
+            array_len: 9,
+            row_len: 3,
+            // num_rows = 9 / 3 = 3
+        };
+        assert_eq!(grid_square.idx_to_coords(0), Some(Coordinates { x: 0, y: 0 }));
+        assert_eq!(grid_square.idx_to_coords(1), Some(Coordinates { x: 1, y: 0 }));
+        assert_eq!(grid_square.idx_to_coords(2), Some(Coordinates { x: 2, y: 0 }));
+        assert_eq!(grid_square.idx_to_coords(3), Some(Coordinates { x: 0, y: 1 }));
+        assert_eq!(grid_square.idx_to_coords(4), Some(Coordinates { x: 1, y: 1 }));
+        assert_eq!(grid_square.idx_to_coords(5), Some(Coordinates { x: 2, y: 1 }));
+        assert_eq!(grid_square.idx_to_coords(6), Some(Coordinates { x: 0, y: 2 }));
+        assert_eq!(grid_square.idx_to_coords(7), Some(Coordinates { x: 1, y: 2 }));
+        assert_eq!(grid_square.idx_to_coords(8), Some(Coordinates { x: 2, y: 2 }));
+    }
+
+    #[test]
+    fn test_coords_to_idx() {
+        let grid = Grid {
+            array_len: 6,
+            row_len: 2,
+        };
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 0, y: 0 }), Some(0));
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 1, y: 0 }), Some(1));
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 0, y: 1 }), Some(2));
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 1, y: 1 }), Some(3));
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 0, y: 2 }), Some(4));
+        assert_eq!(grid.coords_to_idx(&Coordinates { x: 1, y: 2 }), Some(5));
     }
 }
