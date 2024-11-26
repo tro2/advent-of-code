144f6c88ceece9686d12e295d2dc65eb825854cb
tro2 <62850247+tro2@users.noreply.github.com>
2024-11-26 14:58:36 -0800

    finish 2023 day 03 pt 2

--- 2023/crates/day_03_gear_ratios/src/lib.rs
+++ 2023/crates/day_03_gear_ratios/src/lib.rs
@@ -1,71 +1,120 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::{char, fs::read_to_string};
+use shared::{Arrayto2DGrid, Offsets};
 
-pub fn part_01(path: &str) -> u32 {
-    let lines = BufReader::new(File::open(path).unwrap()).lines();
-    let mut symbol_positions = Vec::new();
-    
-    for (line_idx, line) in lines.enumerate() {
-        if let Ok(line) = line {
-            for (char_idx, character) in line.chars().enumerate() {
-                if (character < '0' || character > '9') && character != '.' {
-                    symbol_positions.push((line_idx as i32, char_idx as i32));
-                    // println!("Found symbol ({}) at ({}, {})", character, line_idx, char_idx);
-                }
-            }
-        }
+struct SchematicString<'a> {
+    bytes: &'a [u8],
+    row_len: usize,
+}
+
+impl<'a> SchematicString<'a> {
+    fn new(bytes: &'a [u8], row_len: usize) -> Self {
+        Self { bytes, row_len }
+    }
+}
+
+impl Arrayto2DGrid for SchematicString<'_> {
+    fn array_len(&self) -> usize {
+        self.bytes.len()
     }
 
-    let lines = BufReader::new(File::open(path).unwrap()).lines();
+    fn row_len(&self) -> usize {
+        self.row_len
+    }
+}
+
+fn is_symbol(byte: u8) -> bool {
+    (byte < b'0' || byte > b'9') && byte != b'.' && byte != b'\n'
+}
+
+fn check_neighbors_for_symbol(idx: usize, schematic: &SchematicString) -> bool {
+    Offsets::ALL.iter().any(|offset| {
+        if let Some(t_idx) = schematic.translate_idx(idx, offset) {
+            is_symbol(schematic.bytes[t_idx])
+        } else {
+            false
+        }
+    })
+}
+
+// given an index that is a number, extract and parse that number
+fn parse_num(idx: usize, schematic: &SchematicString) -> u32 {
+    let char = schematic.bytes[idx] as char;
+    if char.to_digit(10) == None {
+        panic!("parse_num called on non-number");
+    }
+
+    // in a valid number
+    let startidx = (0..idx).rev()
+    .find(|&i| (schematic.bytes[i] as char).to_digit(10).is_none())
+    .map_or(0, |i| i + 1);
+
+    let endidx = (idx..schematic.bytes.len())
+    .find(|&i| (schematic.bytes[i] as char).to_digit(10).is_none())
+    .unwrap_or(schematic.array_len());
+
+    std::str::from_utf8(&schematic.bytes[startidx..endidx])
+        .unwrap()
+        .parse::<u32>()
+        .ok()
+        .unwrap()
+}
+
+pub fn part_01(path: &str) -> u32 {
+    let input = read_to_string(path).unwrap();
+    let schematic = SchematicString::new(input.as_bytes(), input.find('\n').unwrap() + 1);
+
     let mut sum = 0;
+    let mut curr_num = 0;
+    let mut found_sym = false;
 
-    for (line_idx, line) in lines.enumerate() {
-        if let Ok(line) = line {
-            let mut num = 0;
-            let mut power = 0;
-            let mut sym_adj = false;
-            let mut num_ended = false;
-            for (rev_char_idx, character) in line.chars().rev().enumerate() {
-                let char_idx = line.len() - rev_char_idx - 1;
-                if character >= '0' && character <= '9' {
-                    if is_symbol_adj(line_idx as i32, char_idx as i32, &symbol_positions) {
-                        sym_adj = true;
-                    }
-                    num += (character as u32 - '0' as u32) * 10u32.pow(power);
-                    power += 1;
-                    if char_idx == 0 {
-                        num_ended = true;
-                    }
-                } else if num != 0 {
-                    num_ended = true;
-                }
-
-                if num_ended && num != 0 {
-                    if sym_adj {
-                        // println!("Found sym_adj number ({}), added to sum for total of {}", num, sum+num);
-                        sum += num;
-                    } else {
-                        // println!("Found non-sym_adj number ({}), resetting", num);
-                    }
-                    num = 0;
-                    power = 0;
-                    num_ended = false;
-                    sym_adj = false;
-                }
+    for (idx, byte) in schematic.bytes.iter().enumerate() {
+        let c = *byte as char;
+        if let Some(digit) = c.to_digit(10) {
+            curr_num = curr_num * 10 + digit;
+            if !found_sym {
+                found_sym = check_neighbors_for_symbol(idx, &schematic);
             }
+        } else {
+            // at end of number
+            if found_sym {
+                sum += curr_num;
+                found_sym = false;
+            }
+            curr_num = 0;
         }
     }
 
     return sum;
 }
 
-fn is_symbol_adj(line_idx: i32, char_idx: i32, symbol_positions: &Vec<(i32, i32)>) -> bool {
-    for &(y, x) in symbol_positions {
-        if (y == line_idx && (x == char_idx - 1 || x == char_idx + 1)) || // left or right
-           (y == line_idx - 1 && (x == char_idx - 1 || x == char_idx || x == char_idx + 1)) || // above
-           (y == line_idx + 1 && (x == char_idx - 1 || x == char_idx || x == char_idx + 1)) { // below
-            return true;
+pub fn part_02(path: &str) -> u32 {
+    let input = read_to_string(path).unwrap();
+    let schematic = SchematicString::new(input.as_bytes(), input.find('\n').unwrap() + 1);
+
+    let mut sum = 0;
+
+    for (idx, byte) in schematic.bytes.iter().enumerate() {
+        if byte != &b'*' {
+            continue;
+        }
+
+        let nums = Offsets::ALL.iter()
+            .filter_map(|offset| {
+                if let Some(t_idx) = schematic.translate_idx(idx, offset) {
+                    if schematic.bytes[t_idx] >= b'0' && schematic.bytes[t_idx] <= b'9' {
+                        return Some(parse_num(t_idx, &schematic));
+                    }
+                }
+                None
+            }).collect::<Vec<_>>();
+        
+        // filter list to unique nums and if there are 2, multiply them together and add to sum
+        let unique_nums = nums.iter().cloned().collect::<std::collections::HashSet<_>>();
+        if unique_nums.len() == 2 {
+            println!("{:?}", unique_nums);
+            sum += unique_nums.iter().product::<u32>();
         }
     }
-    false
+
+    sum
 }
--- 2023/crates/day_03_gear_ratios/tests/day_03_test.rs
+++ 2023/crates/day_03_gear_ratios/tests/day_03_test.rs
@@ -9,3 +9,13 @@ fn d3_part_01_example() {
 fn d3_part_01_input() {
     assert_eq!(day_03::part_01("tests/day_03_input.txt"), 514969);
 }
+
+#[test]
+fn d3_part_02_example() {
+    assert_eq!(day_03::part_02("tests/day_03_example.txt"), 467835);
+}
+
+#[test]
+fn d3_part_02_input() {
+    assert_eq!(day_03::part_02("tests/day_03_input.txt"), 78915902);
+}
