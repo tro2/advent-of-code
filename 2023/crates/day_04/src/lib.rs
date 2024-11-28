4b0d5fe1c2dd3b2b647c34400ecb18cde28cc2a4
tro2 <62850247+tro2@users.noreply.github.com>
2024-11-27 18:32:11 -0800

    finished 2023 day 4 pt 2

--- 2023/crates/day_04/src/lib.rs
+++ 2023/crates/day_04/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -21,6 +22,36 @@ pub fn part_01(path: &str) -> u32 {
     }
     
     sum
+}
+
+pub fn part_02(path: &str) -> u32 {
+    let source = BufReader::new(File::open(path).unwrap()).lines();
+    let mut card_counts = HashMap::<u32, u32>::new();
+
+    for line in source.filter_map(Result::ok) {
+        // Line format
+        // Game 1: winningnum1 winningnum2 winningnum3 ... | num1 num2 ...
+        let (id, game) = line.split_once(":").unwrap();
+        
+        // id: "Card 1383"
+        let game_id = id.split_whitespace().last().unwrap().parse::<u32>().unwrap();
+
+        // determine count of winning nums on card
+        let (win_nums, found_nums) = game.split_once("|").unwrap();
+        let winning_nums = extract_nums(&win_nums[1..]);
+        let found_nums = extract_nums(&found_nums[1..]);
+        let win_count = get_win_count(&winning_nums, &found_nums);
+
+        // update card counts
+        let num_copies = *card_counts.get(&game_id).unwrap_or(&0) + 1;
+        card_counts.insert(game_id, num_copies);
+        for i in game_id + 1..game_id + win_count + 1 {
+            let new_count = num_copies + card_counts.get(&i).unwrap_or(&0);
+            card_counts.insert(i, new_count);
+        }
+    }
+
+    card_counts.values().sum()
 }
 
 // takes a string with space separated unsigned numbers and returns them in a vector
--- 2023/crates/day_04/tests/d4_test.rs
+++ 2023/crates/day_04/tests/d4_test.rs
@@ -9,3 +9,13 @@ fn d4_part_01_example() {
 fn d4_part_01_input() {
     assert_eq!(day_04::part_01("tests/d4_input.txt"), 20667);
 }
+
+#[test]
+fn d4_part_02_example() {
+    assert_eq!(day_04::part_02("tests/d4_example.txt"), 30);
+}
+
+#[test]
+fn d4_part_02_input() {
+    assert_eq!(day_04::part_02("tests/d4_input.txt"), 5833065);
+}
