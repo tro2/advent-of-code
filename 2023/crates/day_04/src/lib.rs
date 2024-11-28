0b991617c7e7d5dd677d80c05af244c373b9a1f5
tro2 <62850247+tro2@users.noreply.github.com>
2024-11-27 17:16:30 -0800

    extracted funcs for 2023 d4 pt 2 and refactored

--- 2023/crates/day_04/src/lib.rs
+++ 2023/crates/day_04/src/lib.rs
@@ -5,19 +5,15 @@ pub fn part_01(path: &str) -> u32 {
     let source = BufReader::new(File::open(path).unwrap()).lines();
     let mut sum = 0;
 
-    for line in source {
-        if line.is_err() {
-            break;
-        }
-        let line = line.unwrap();
-
+    for line in source.filter_map(Result::ok) {
         // Line format
         // Game 1: winningnum1 winningnum2 winningnum3 ... | num1 num2 ...
-        let (_id, game) = line.split_at(line.find(":").unwrap());
-        let (winning_nums, nums) = game.split_at(game.find("|").unwrap());
-        let win_nums = extract_nums(&winning_nums[1..]);
-        let found_nums = extract_nums(&nums[1..]);
-        let count = win_nums.iter().filter(|num| found_nums.contains(num)).count() as u32;
+        let (_id, game) = line.split_once(":").unwrap();
+        let (win_nums, found_nums) = game.split_once("|").unwrap();
+
+        let winning_nums = extract_nums(&win_nums[1..]);
+        let found_nums = extract_nums(&found_nums[1..]);
+        let count = get_win_count(&winning_nums, &found_nums);
 
         if count > 0 {
             sum += 2_u32.pow(count-1);
@@ -27,9 +23,14 @@ pub fn part_01(path: &str) -> u32 {
     sum
 }
 
+// takes a string with space separated unsigned numbers and returns them in a vector
+// Example Input: " 83 273 93 10 28 1 82 "
 fn extract_nums(source: &str) -> Vec<u32> {
-    source.trim().split(' ')
-        .filter(|p| !p.is_empty())
-        .map(|s| s.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>()
+    source.split_whitespace()
+        .filter_map(|s| s.parse::<u32>().ok())
+        .collect()
+}
+
+fn get_win_count(win_nums: &[u32], found_nums: &[u32]) -> u32 {
+    win_nums.iter().filter(|&num| found_nums.contains(num)).count() as u32
 }
--- 2023/crates/day_04/tests/d4_test.rs
+++ 2023/crates/day_04/tests/d4_test.rs
@@ -2,10 +2,10 @@ use day_04;
 
 #[test]
 fn d4_part_01_example() {
-    assert_eq!(day_04::part_01("tests/day_04_example.txt"), 13);
+    assert_eq!(day_04::part_01("tests/d4_example.txt"), 13);
 }
 
 #[test]
 fn d4_part_01_input() {
-    assert_eq!(day_04::part_01("tests/day_04_input.txt"), 20667);
+    assert_eq!(day_04::part_01("tests/d4_input.txt"), 20667);
 }
