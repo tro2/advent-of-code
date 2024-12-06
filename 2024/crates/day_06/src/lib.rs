39a168808f47151d6b608ea1099cd8906574ff83
tro2 <62850247+tro2@users.noreply.github.com>
2024-12-05 21:33:02 -0800

    init 2024 day 6

---
+++ 2024/crates/day_06/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn part_01(path: &str) -> u32 {
+    0
+}
+
+pub fn part_02(path: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        
+    }
+}
---
+++ 2024/crates/day_06/tests/test.rs
@@ -0,0 +1,21 @@
+use day_06;
+
+#[test]
+fn d6_p1_example() {
+    assert_eq!(day_06::part_01("tests/example.txt"), 41)
+}
+
+#[test]
+fn d6_p1_input() {
+    assert_eq!(day_06::part_01("tests/input.txt"), 41)
+}
+
+#[test]
+fn d6_p2_example() {
+    assert_eq!(day_06::part_02("tests/example.txt"), 41)
+}
+
+#[test]
+fn d6_p2_input() {
+    assert_eq!(day_06::part_02("tests/input.txt"), 41)
+}
