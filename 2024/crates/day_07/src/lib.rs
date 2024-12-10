3c750a279b30642a7e7ae1964530dd27508a85fa
tro2 <62850247+tro2@users.noreply.github.com>
2024-12-09 18:51:40 -0800

    finished 2024 day 7

--- 2024/crates/day_07/src/lib.rs
+++ 2024/crates/day_07/src/lib.rs
@@ -1,9 +1,89 @@
-pub fn part_01(path: &str) -> u32 {
-    0
+use std::fs::read_to_string;
+
+pub fn part_01(path: &str) -> usize {
+    let source = read_to_string(path).unwrap();
+    let mut sum = 0;
+
+    for line in source.lines() {
+        let (answer, nums) = line.split_once(':').unwrap();
+        let answer: usize = answer.parse().unwrap();
+        let nums: Vec<usize> = nums.split_whitespace()
+            .rev()
+            .map(|num| num.parse().unwrap())
+            .collect();
+
+        if is_valid(answer, &nums) {
+            sum += answer;
+        }
+    }
+
+    return sum;
+
+    fn is_valid(answer: usize, nums: &Vec<usize>) -> bool {
+        if nums.len() == 1 {
+            return answer == nums[0];
+        }
+    
+        let mut add = nums.to_owned();
+        let sum = add.pop().unwrap() + add.pop().unwrap();
+        add.push(sum);
+    
+        let mut mult = nums.to_owned();
+        let product = mult.pop().unwrap() * mult.pop().unwrap();
+        mult.push(product);
+    
+        if sum <= answer && is_valid(answer, &add) || product <= answer && is_valid(answer, &mult) {
+            return true;
+        }
+        false
+    }
 }
 
-pub fn part_02(path: &str) -> u32 {
-    0
+pub fn part_02(path: &str) -> usize {
+    let source = read_to_string(path).unwrap();
+    let mut sum = 0;
+
+    for line in source.lines() {
+        let (answer, nums) = line.split_once(':').unwrap();
+        let answer: usize = answer.parse().unwrap();
+        let nums: Vec<usize> = nums.split_whitespace()
+            .rev()
+            .map(|num| num.parse().unwrap())
+            .collect();
+
+        if is_valid(answer, &nums) {
+            sum += answer;
+        }
+    }
+
+    return sum;
+
+    fn is_valid(answer: usize, nums: &Vec<usize>) -> bool {
+        if nums.len() == 1 {
+            return answer == nums[0];
+        }
+    
+        let mut add = nums.to_owned();
+        let sum = add.pop().unwrap() + add.pop().unwrap();
+        add.push(sum);
+    
+        let mut mult = nums.to_owned();
+        let product = mult.pop().unwrap() * mult.pop().unwrap();
+        mult.push(product);
+    
+        let mut comb = nums.to_owned();
+        let a = comb.pop().unwrap();
+        let b = comb.pop().unwrap();
+        let combination: usize = format!("{}{}", a, b).parse().unwrap();
+        comb.push(combination);
+    
+        if  sum <= answer && is_valid(answer, &add) ||
+            product <= answer && is_valid(answer, &mult) ||
+            combination <= answer && is_valid(answer, &comb) {
+            return true;
+        }
+        false
+    }
 }
 
 #[cfg(test)]
--- 2024/crates/day_07/tests/test.rs
+++ 2024/crates/day_07/tests/test.rs
@@ -7,15 +7,15 @@ fn d7_p1_example() {
 
 #[test]
 fn d7_p1_input() {
-    assert_eq!(day_07::part_01("tests/input.txt"), 5208)
+    assert_eq!(day_07::part_01("tests/input.txt"), 1611660863222)
 }
 
 #[test]
 fn d7_p2_example() {
-    assert_eq!(day_07::part_02("tests/example.txt"), 6)
+    assert_eq!(day_07::part_02("tests/example.txt"), 11387)
 }
 
 #[test]
 fn d7_p2_input() {
-    assert_eq!(day_07::part_02("tests/input.txt"), 1972)
+    assert_eq!(day_07::part_02("tests/input.txt"), 945341732469724)
 }
